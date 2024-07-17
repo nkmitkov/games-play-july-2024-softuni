8ec1246686afcafb228731425f1e302210f7356f
Nick <nkmitkov@gmail.com>
2024-07-17 13:13:31 +0300

    Add games view and create game view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import GameList from "./components/game-list/GameList";
+import GameCreate from "./components/game-create/GameCreate";
 
 function App() {
 
@@ -9,7 +13,11 @@ function App() {
 
             <main id="main-content">
 
-                <Home />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/games" element={<GameList />} />
+                    <Route path="/games/create" element={<GameCreate />} />
+                </Routes>
 
             </main>
         </div>
