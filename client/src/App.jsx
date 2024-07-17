6b8db5b6f72a001383a8981a6f17a7aee3dcbdcc
Nick <nkmitkov@gmail.com>
2024-07-17 12:48:04 +0300

    Add home view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
 
     return (
         <div id="box">
             <Header />
+
+            <main id="main-content">
+
+                <Home />
+
+            </main>
         </div>
     )
 }
