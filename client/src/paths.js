6a93b520cb26af679e6709ed345d8aa3d1c70292
Nick <nkmitkov@gmail.com>
2024-07-24 10:59:28 +0300

    Add paths

---
+++ client/src/paths.js
@@ -0,0 +1,10 @@
+const Path = {
+    Home: "/",
+    Games: "/games",
+    Create: "/games/create",
+    Details: "/games/:id",
+    Login: "/login",
+    Register: "/register",
+};
+
+export default Path;
