9393fca8b8a4545677d1d08c494b1961b2b00c8e
Nick <nkmitkov@gmail.com>
2024-08-03 21:01:44 +0300

    Add code-splitting bundling with Suspense and React.lazy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./contexts/authContext";
@@ -7,7 +8,6 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import GameList from "./components/game-list/GameList";
 import GameCreate from "./components/game-create/GameCreate";
-import GameDetails from "./components/game-details/GameDetails";
 import GameEdit from "./components/game-edit/GameEdit";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
@@ -15,7 +15,8 @@ import Logout from "./components/logout/Logout";
 import ErrorBoundary from "./components/ErrorBoundary";
 import AuthGuard from "./components/guards/AuthGuard";
 import GuestGuard from "./components/guards/GuestGuard";
-// import BasicAuthGuard from "./components/guards/BasicAuthGuard";
+// import GameDetails from "./components/game-details/GameDetails";
+const GameDetails = lazy(() => import("./components/game-details/GameDetails"));
 
 function App() {
 
@@ -27,22 +28,24 @@ function App() {
 
                     <main id="main-content">
 
-                        <Routes>
-                            <Route path={Path.Home} element={<Home />} />
-                            <Route path={Path.Catalog} element={<GameList />} />
-                            <Route path={Path.Details} element={<GameDetails />} />
+                        <Suspense fallback={<h1>Loading...</h1>}>
+                            <Routes>
+                                <Route path={Path.Home} element={<Home />} />
+                                <Route path={Path.Catalog} element={<GameList />} />
+                                <Route path={Path.Details} element={<GameDetails />} />
 
-                            <Route element={<AuthGuard />}>
-                                <Route path={Path.Create} element={<GameCreate />} />
-                                <Route path={Path.GameEdit} element={<GameEdit />} />
-                                <Route path={Path.Logout} element={<Logout />} />
-                            </Route>
+                                <Route element={<AuthGuard />}>
+                                    <Route path={Path.Create} element={<GameCreate />} />
+                                    <Route path={Path.GameEdit} element={<GameEdit />} />
+                                    <Route path={Path.Logout} element={<Logout />} />
+                                </Route>
 
-                            <Route element={<GuestGuard />}>
-                                <Route path={Path.Login} element={<Login />} />
-                                <Route path={Path.Register} element={<Register />} />
-                            </Route>
-                        </Routes>
+                                <Route element={<GuestGuard />}>
+                                    <Route path={Path.Login} element={<Login />} />
+                                    <Route path={Path.Register} element={<Register />} />
+                                </Route>
+                            </Routes>
+                        </Suspense>
 
                     </main>
                 </div>
